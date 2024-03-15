e06bc48776160f33c43f8af0cf4b319bd6a3965b
root@localhost <134883592+intelxd86@users.noreply.github.com>
2024-03-15 11:21:49 +0100

    user create and login controllers

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateUserRequest;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserController extends Controller
+{
+    public function createUser(CreateUserRequest $request)
+    {
+        $user = new User();
+        $user->uuid = Str::uuid();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+
+        return response()->json(['user' => $user->uuid]);
+    }
+
+    public function loginUser(LoginRequest $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['error' => 'Invalid credentials'], 401);
+        }
+
+        return response()->json(['user' => Auth::user()->uuid]);
+    }
+}
